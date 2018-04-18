bd4f611edbcb626cacec53b2660893c832fa3df9
Julie <jgelman@hawk.iit.edu>
2018-04-18 14:24:51 -0500

    trying to fix javascript

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ function initMap() {
   map: map
   });
 }
+
+function goto(location, title){
+  map.panTo(location);
+  map.setZoom(12);
+  marker = new google.maps.Marker({
+  position: location,
+  map: map
+  });
+  if (title==places[0]){
+    document.getElementById("demo").innerHTML = "Chicago, IL: A beautiful city in the midwest. I moved out here to attend Illinois Tech two years ago.";
+    }
+  else if (title==places[1]){
+    document.getElementById("demo").innerHTML = "Philadelphia, PA: My hometown, a small city with great food and rich history.";
+    }
+  else if (title==places[2]){
+    document.getElementById("demo").innerHTML = "Paris, France: One of the most beautiful places I have ever visited, a truly magical place.";
+    }
+  else if (title==places[3]){
+    document.getElementById("demo").innerHTML = "Athens, Greece: Another one of my favorite places, very large with historical beauty in every direction.";
+    }
+  else
+    {
+    document.getElementById("demo").innerHTML = null;
+    }
+}
