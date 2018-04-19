70c418221d29da85d0d6914537c58421368528bd
Julie <jgelman@hawk.iit.edu>
2018-04-18 22:51:29 -0500

    trying to center slider

--- javascript/scriptnew.js
+++ javascript/scriptnew.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
          $('.slider').bxSlider({
+
      mode: 'fade',
      captions: true,
      slideWidth: 800,
+     slider: center;
    responsive: true
      });
    });
